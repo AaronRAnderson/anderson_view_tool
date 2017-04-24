7139b8c1e6a4707c9b78b20cf10d0295c84534c8
Aaron Anderson <aaron.russell.anderson@gmail.com>
2017-04-24 13:42:05 -0600

    Dev Tool Initial commit

---
+++ lib/anderson_view_tool.rb
@@ -0,0 +1,6 @@
+require "anderson_view_tool/version"
+require "anderson_view_tool/renderer"
+
+module AndersonViewTool
+  # Your code goes here...
+end
---
+++ spec/anderson_view_tool_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe AndersonViewTool do
+  it "has a version number" do
+    expect(AndersonViewTool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
